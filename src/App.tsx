19b3bccc6fa8f316d1a85b11546d24bb42f8bbe8
Alexey Butkeev <git@abutkeev.ru>
2022-12-22 18:09:29 +0300

    :sparkles: add frontend boilerplate; ref T8215
    Auditors: cos

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+const App: React.FC = () => {
+  return <h1 style={{ textAlign: 'center' }}>App template</h1>;
+};
+
+export default App;

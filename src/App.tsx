167443f9e6bd7ead62818d589fd3030b01985b99
Alexey Butkeev <git@abutkeev.ru>
2023-01-11 08:53:14 +0300

    :fire: remove prettier and roboto font; ref T8215
    Auditors: cos

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,4 @@
 import React from 'react';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
 
 const App: React.FC = () => {
   return <h1 style={{ textAlign: 'center' }}>App template</h1>;

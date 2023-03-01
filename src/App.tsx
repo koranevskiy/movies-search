c09fd1d6ee383859a626ce3e114054ff78820701
koranevskiy <v.koranevskiy@gmail.com>
2023-03-01 14:39:04 +0300

    configured eslint prettier husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from 'react'
 
 const App: React.FC = () => {
-  return <h1 style={{ textAlign: 'center' }}>App template</h1>;
-};
+  return <h1 style={{ textAlign: 'center' }}>App asd</h1>
+}
 
-export default App;
+export default App
